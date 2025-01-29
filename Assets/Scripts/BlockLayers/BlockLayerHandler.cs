74d34dc26892729ce1cfda43d5605a9885778bb8
Slayer-Quanta <91901675+Slayer-Quanta@users.noreply.github.com>
2025-01-28 22:18:21 -0500

    Procedural Generation completed, there is some player stuff needed but will do that later

---
+++ Assets/Scripts/BlockLayers/BlockLayerHandler.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public abstract class BlockLayerHandler : MonoBehaviour
+{
+    [SerializeField]
+    private BlockLayerHandler Next;
+    public bool Handle(ChunkData chunkData, int x, int y, int z, int surfaceHeightNoise, Vector2Int mapSeedOffset)
+    {
+        if (TryHandling(chunkData, x, y, z, surfaceHeightNoise, mapSeedOffset))
+            return true;
+        if (Next != null)
+            return Next.Handle(chunkData, x,y, z, surfaceHeightNoise, mapSeedOffset);
+        return false;
+    }
+
+    protected abstract bool TryHandling(ChunkData chunkData, int x, int y, int z, int surfaceHeightNoise, Vector2Int mapSeedOffset);
+}
---
+++ Assets/Scripts/World/WorldDataHelper.cs
@@ -0,0 +1,108 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class WorldDataHelper
+{
+    public static Vector3Int ChunkPositionFromBlockCoords(World world, Vector3Int position)
+    {
+        return new Vector3Int
+        {
+            x = Mathf.FloorToInt(position.x / (float)world.chunkSize) * world.chunkSize,
+            y = Mathf.FloorToInt(position.y / (float)world.chunkHeight) * world.chunkHeight,
+            z = Mathf.FloorToInt(position.z / (float)world.chunkSize) * world.chunkSize
+        };
+    }
+
+    internal static List<Vector3Int> GetChunkPositionsAroundPlayer(World world, Vector3Int playerPosition)
+    {
+        int startX = playerPosition.x - (world.chunkDrawingRange) * world.chunkSize;
+        int startZ = playerPosition.z - (world.chunkDrawingRange) * world.chunkSize;
+        int endX = playerPosition.x + (world.chunkDrawingRange) * world.chunkSize;
+        int endZ = playerPosition.z + (world.chunkDrawingRange) * world.chunkSize;
+
+        List<Vector3Int> chunkPositionsToCreate = new List<Vector3Int>();
+        for (int x = startX; x <= endX; x += world.chunkSize)
+        {
+            for (int z = startZ; z <= endZ; z += world.chunkSize)
+            {
+                Vector3Int chunkPos = ChunkPositionFromBlockCoords(world, new Vector3Int(x, 0, z));
+                chunkPositionsToCreate.Add(chunkPos);
+            }
+        }
+        return chunkPositionsToCreate;
+    }
+
+    internal static void RemoveChunkData(World world, Vector3Int pos)
+    {
+        world.worldData.chunkDataDictionary.Remove(pos);
+    }
+
+    internal static void RemoveChunk(World world, Vector3Int pos)
+    {
+        ChunkRenderer chunk = null;
+        if (world.worldData.chunkDictionary.TryGetValue(pos, out chunk))
+        {
+            world.RemoveChunk(chunk);
+            world.worldData.chunkDictionary.Remove(pos);
+        }
+    }
+
+    internal static List<Vector3Int> GetDataPositionsAroundPlayer(World world, Vector3Int playerPosition)
+    {
+        int startX = playerPosition.x - (world.chunkDrawingRange + 1) * world.chunkSize;
+        int startZ = playerPosition.z - (world.chunkDrawingRange + 1) * world.chunkSize;
+        int endX = playerPosition.x + (world.chunkDrawingRange + 1) * world.chunkSize;
+        int endZ = playerPosition.z + (world.chunkDrawingRange + 1) * world.chunkSize;
+
+        List<Vector3Int> chunkDataPositionsToCreate = new List<Vector3Int>();
+        for (int x = startX; x <= endX; x += world.chunkSize)
+        {
+            for (int z = startZ; z <= endZ; z += world.chunkSize)
+            {
+                Vector3Int chunkPos = ChunkPositionFromBlockCoords(world, new Vector3Int(x, 0, z));
+                chunkDataPositionsToCreate.Add(chunkPos);
+            }
+        }
+
+        return chunkDataPositionsToCreate;
+    }
+
+    internal static List<Vector3Int> GetUnnededData(World.WorldData worldData, List<Vector3Int> allChunkDataPositionsNeeded)
+    {
+        return worldData.chunkDataDictionary.Keys
+    .Where(pos => allChunkDataPositionsNeeded.Contains(pos) == false && worldData.chunkDataDictionary[pos].modifiedByThePlayer == false)
+    .ToList();
+
+    }
+
+    internal static List<Vector3Int> GetUnnededChunks(World.WorldData worldData, List<Vector3Int> allChunkPositionsNeeded)
+    {
+        List<Vector3Int> positionToRemove = new List<Vector3Int>();
+        foreach (var pos in worldData.chunkDictionary.Keys
+            .Where(pos => allChunkPositionsNeeded.Contains(pos) == false))
+        {
+            if (worldData.chunkDictionary.ContainsKey(pos))
+            {
+                positionToRemove.Add(pos);
+
+            }
+        }
+        return positionToRemove;
+    }
+
+    internal static List<Vector3Int> SelectPositonsToCreate(World.WorldData worldData, List<Vector3Int> allChunkPositionsNeeded, Vector3Int playerPosition)
+    {
+        return allChunkPositionsNeeded.Where(pos => worldData.chunkDictionary.ContainsKey(pos) == false)
+            .OrderBy(pos => Vector3.Distance(playerPosition, pos))
+            .ToList();
+    }
+
+    internal static List<Vector3Int> SelectDataPositonsToCreate(World.WorldData worldData, List<Vector3Int> allChunkDataPositionsNeeded, Vector3Int playerPosition)
+    {
+        return allChunkDataPositionsNeeded
+            .Where(pos => worldData.chunkDataDictionary.ContainsKey(pos) == false)
+            .OrderBy(pos => Vector3.Distance(playerPosition, pos))
+            .ToList();
+    }
+}
