c3e783ce8e2a02cc4c753f0276b172d644ca0055
L U F i A S <myenuddinkabir.ricko@georgebrown.ca>
2025-02-22 00:40:36 +0600

    enemy health system

--- Assets/_Project/_Script/Enemy.cs
+++ Assets/_Project/_Script/Enemy.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
+    [Header("Health")]
+    public float initialHealth = 100;
+    public float currentHealth;
+    public Image healthFill;
+
+    [Space(20)]
     public Transform player;
     public NavMeshAgent agent;
     public float updateRate = 1f;
+
 
     public void Init(Transform player)
     {
         this.player = player;
         enabled = true;
+        initialHealth = currentHealth;
 
         InvokeRepeating(nameof(UpdatePlayerDetection), 0, updateRate);
     }
@@ -18,5 +27,27 @@ public class Enemy : MonoBehaviour
     private void UpdatePlayerDetection()
     {
         agent.SetDestination(player.position);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        healthFill.fillAmount = currentHealth / initialHealth;
+        if (currentHealth <= 0)
+        {
+            Dead();
+        }
+    }
+
+    void Dead()
+    {
+        Destroy(gameObject);
+    }
+
+
+    [ButtonLUFI]
+    void TestTakeDamage()
+    {
+        TakeDamage(10);
     }
 }
