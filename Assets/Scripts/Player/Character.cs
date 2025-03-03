1665a9b57fd401a86fe27a2552b51cb5cb925824
Slayer-Quanta <91901675+Slayer-Quanta@users.noreply.github.com>
2025-03-02 21:42:30 -0500

    Fixes has been added imma merge this into current main

--- Assets/Scripts/Player/Character.cs
+++ Assets/Scripts/Player/Character.cs
@@ -4,38 +4,36 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-	[SerializeField] private Camera mainCamera;
-	[SerializeField] private PlayerInput playerInput;
-	[SerializeField] private PlayerMovement playerMovement;
-
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private PlayerMovement playerMovement;
     [SerializeField] private PauseSystem pauseMenu;
+    [SerializeField] private GameObject explosionPrefab;
+
     public float interactionRayLength = 5;
     public LayerMask groundMask;
-	public bool fly = false;
-	public Animator animator;
-	bool isWaiting = false;
-	public World world;
+    public bool fly = false;
+    public Animator animator;
+    bool isWaiting = false;
+    public World world;
 
-	private float lastClickTime = 0f;
-	private float doubleClickThreshold = 0.5f; // Time window for double-click in seconds
-	private Vector3Int lastClickedBlockPos = Vector3Int.zero;
+    private float lastClickTime = 0f;
+    private float doubleClickThreshold = 0.5f;
+    private Vector3Int lastClickedBlockPos = Vector3Int.zero;
 
+    private void Awake()
+    {
+        if (mainCamera == null)
+            mainCamera = Camera.main;
+        playerInput = GetComponent<PlayerInput>();
+        playerMovement = GetComponent<PlayerMovement>();
+        world = FindObjectOfType<World>();
+    }
 
-	[SerializeField] private GameObject explosionPrefab; // Explosion effect prefab
-
-	private void Awake()
-	{
-		if (mainCamera == null)
-			mainCamera = Camera.main;
-		playerInput = GetComponent<PlayerInput>();
-		playerMovement = GetComponent<PlayerMovement>();
-		world = FindObjectOfType<World>();
-	}
-
-	private void Start()
-	{
-		playerInput.OnMouseClick += HandleMouseClick;
-		playerInput.OnFly += HandleFlyClick;
+    private void Start()
+    {
+        playerInput.OnMouseClick += HandleMouseClick;
+        playerInput.OnFly += HandleFlyClick;
 
         pauseMenu = FindObjectOfType<PauseSystem>();
         playerInput.OnPause += pauseMenu.TogglePause;
@@ -51,9 +49,9 @@ public class Character : MonoBehaviour
     }
 
     private void HandleFlyClick()
-	{
-		fly = !fly;
-	}
+    {
+        fly = !fly;
+    }
 
     void Update()
     {
@@ -68,10 +66,10 @@ public class Character : MonoBehaviour
         {
             animator.SetBool("isGrounded", playerMovement.IsGrounded);
 
-            if (playerMovement.IsGrounded && playerInput.IsJumping && isWaiting == false)
+            if (playerMovement.IsGrounded && playerInput.IsJumping && !isWaiting)
             {
                 animator.SetTrigger("jump");
-                AudioManager.instance.PlayJumpSound(); 
+                AudioManager.instance.PlayJumpSound();
                 isWaiting = true;
                 StopAllCoroutines();
                 StartCoroutine(ResetWaiting());
@@ -81,7 +79,7 @@ public class Character : MonoBehaviour
 
             if (playerMovement.IsGrounded && playerInput.MovementInput.magnitude > 0)
             {
-                if (!AudioManager.instance.sfxSource.isPlaying) 
+                if (!AudioManager.instance.sfxSource.isPlaying)
                     AudioManager.instance.PlayWalkSound();
             }
 
@@ -90,75 +88,98 @@ public class Character : MonoBehaviour
         }
     }
 
-
     IEnumerator ResetWaiting()
-	{
-		yield return new WaitForSeconds(0.1f);
-		animator.ResetTrigger("jump");
-		isWaiting = false;
-	}
+    {
+        yield return new WaitForSeconds(0.1f);
+        animator.ResetTrigger("jump");
+        isWaiting = false;
+    }
 
-	private void HandleMouseClick()
-	{
-		Ray playerRay = new Ray(mainCamera.transform.position, mainCamera.transform.forward);
-		RaycastHit hit;
+    private void HandleMouseClick()
+    {
+        AudioManager.instance.PlayButtonClick();
+        Ray playerRay = new Ray(mainCamera.transform.position, mainCamera.transform.forward);
+        RaycastHit hit;
 
-		if (Physics.Raycast(playerRay, out hit, interactionRayLength, groundMask))
-		{
-			Vector3Int clickedBlockPos = new Vector3Int(
-				Mathf.RoundToInt(hit.point.x),
-				Mathf.RoundToInt(hit.point.y),
-				Mathf.RoundToInt(hit.point.z)
-			);
+        if (Physics.Raycast(playerRay, out hit, interactionRayLength, groundMask))
+        {
+            Vector3Int clickedBlockPos = new Vector3Int(
+                Mathf.RoundToInt(hit.point.x),
+                Mathf.RoundToInt(hit.point.y),
+                Mathf.RoundToInt(hit.point.z)
+            );
 
-			// Check if the clicked block is the same as the last one
-			if (clickedBlockPos == lastClickedBlockPos && Time.time - lastClickTime <= doubleClickThreshold)
-			{
-				// Double-clicked, destroy the block and play explosion
-				ModifyTerrain(hit);
-			}
-			else
-			{
-				// First click, record the time and block position
-				lastClickedBlockPos = clickedBlockPos;
-				lastClickTime = Time.time;
-			}
-		}
-	}
+            if (Input.GetMouseButton(0)) // Left-click: Destroy block
+            {
+                if (clickedBlockPos == lastClickedBlockPos && Time.time - lastClickTime <= doubleClickThreshold)
+                {
+                    ModifyTerrain(hit);
+                }
+                else
+                {
+                    lastClickedBlockPos = clickedBlockPos;
+                    lastClickTime = Time.time;
+                }
+            }
+            else if (Input.GetMouseButton(1)) // Right-click: Place block
+            {
+                BlockType lookedAtBlockType = GetLookedAtBlockType(hit);
+                BlockType blockToPlace = GetNextBlockType(lookedAtBlockType);
 
-	private void ModifyTerrain(RaycastHit hit)
-	{
-		// Play explosion effect at the block's position
-		PlayExplosion(hit.point);
+                Vector3Int targetBlockPos = new Vector3Int(
+                    Mathf.FloorToInt(hit.point.x - hit.normal.x * 0.5f),
+                    Mathf.FloorToInt(hit.point.y - hit.normal.y * 0.5f),
+                    Mathf.FloorToInt(hit.point.z - hit.normal.z * 0.5f)
+                );
+                Vector3Int placeBlockPos = targetBlockPos + Vector3Int.RoundToInt(hit.normal);
 
-		// Destroy the block
-		world.SetBlock(hit, BlockType.Air);
-	}
+                BlockType existingBlock = world.GetBlockFromChunkCoordinates(
+                    hit.collider.GetComponent<ChunkRenderer>().ChunkData,
+                    placeBlockPos.x, placeBlockPos.y, placeBlockPos.z
+                );
 
-	private void PlayExplosion(Vector3 position)
-	{
-		if (explosionPrefab != null)
-		{
-			// Instantiate explosion effect at the block’s position
-			GameObject explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+                if (existingBlock == BlockType.Air || existingBlock == BlockType.Nothing)
+                {
+                    world.SetBlock(placeBlockPos, blockToPlace);
+                }
+            }
+        }
+    }
 
-			// Get the Particle System from the explosion prefab
-			ParticleSystem particles = explosion.GetComponent<ParticleSystem>();
+    private BlockType GetNextBlockType(BlockType currentBlock)
+    {
+        switch (currentBlock)
+        {
+            case BlockType.Grass_Dirt: return BlockType.Dirt;
+            case BlockType.Dirt: return BlockType.Stone;
+            case BlockType.Stone: return BlockType.TreeTrunk;
+            case BlockType.TreeTrunk: return BlockType.Grass_Dirt;
+            default: return BlockType.Grass_Dirt;
+        }
+    }
 
-			// Play the particle system
-			particles.Play();
+    private void ModifyTerrain(RaycastHit hit)
+    {
+        PlayExplosion(hit.point);
+        world.SetBlock(hit, BlockType.Air);
+    }
 
-			// Destroy explosion effect after it finishes playing
-			Destroy(explosion, particles.main.duration);
-		}
-	}
+    private void PlayExplosion(Vector3 position)
+    {
+        if (explosionPrefab != null)
+        {
+            GameObject explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+            ParticleSystem particles = explosion.GetComponent<ParticleSystem>();
+            particles.Play();
+            Destroy(explosion, particles.main.duration);
+        }
+    }
 
-	private void ToggleInventory()
+    private void ToggleInventory()
     {
         AudioManager.instance.PlaySFX("Inventory Toggle");
     }
 
-   
     private BlockType GetLookedAtBlockType(RaycastHit hit)
     {
         Vector3Int blockPos = new Vector3Int(
@@ -166,12 +187,9 @@ public class Character : MonoBehaviour
             Mathf.RoundToInt(hit.point.y - hit.normal.y / 2),
             Mathf.RoundToInt(hit.point.z - hit.normal.z / 2)
         );
-
-        // Get the block type at the clicked position
         return world.GetBlockFromChunkCoordinates(
             hit.collider.GetComponent<ChunkRenderer>().ChunkData,
             blockPos.x, blockPos.y, blockPos.z
         );
     }
-
 }
