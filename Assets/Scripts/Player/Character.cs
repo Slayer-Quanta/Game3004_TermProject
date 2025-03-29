bbb3950693a0ebd0a4de5d73e37fe9994e047621
RickoDxD <74686966+RickoDxD@users.noreply.github.com>
2025-03-30 05:03:02 +0600

    Touch And Gamepad Input Added

--- Assets/Scripts/Player/Character.cs
+++ Assets/Scripts/Player/Character.cs
@@ -76,9 +76,20 @@ public class Character : MonoBehaviour
 		Ray playerRay = new Ray(mainCamera.transform.position, mainCamera.transform.forward);
 		RaycastHit hit;
 
+<<<<<<< Updated upstream
 		if (Physics.Raycast(playerRay, out hit, interactionRayLength, groundMask))
 		{
 			Vector3Int clickedBlockPos = new Vector3Int(Mathf.RoundToInt(hit.point.x), Mathf.RoundToInt(hit.point.y), Mathf.RoundToInt(hit.point.z));
+=======
+            if (playerMovement.IsGrounded && playerInput.IsJumping && !isWaiting)
+            {
+                animator.SetTrigger("jump");
+                //AudioManager.instance.PlayJumpSound();
+                isWaiting = true;
+                StopAllCoroutines();
+                StartCoroutine(ResetWaiting());
+            }
+>>>>>>> Stashed changes
 
 			// Check if the clicked block is the same as the last one
 			if (clickedBlockPos == lastClickedBlockPos && Time.time - lastClickTime <= doubleClickThreshold)
@@ -95,8 +106,126 @@ public class Character : MonoBehaviour
 		}
 	}
 
+<<<<<<< Updated upstream
 	private void ModifyTerrain(RaycastHit hit)
 	{
 		world.SetBlock(hit, BlockType.Air);
 	}
+=======
+            if (playerMovement.IsGrounded && playerInput.MovementInput.magnitude > 0)
+            {
+                //if (!AudioManager.instance.sfxSource.isPlaying)
+                //    AudioManager.instance.PlayWalkSound();
+            }
+
+            playerMovement.HandleGravity(playerInput.IsJumping);
+            playerMovement.Walk(playerInput.MovementInput, playerInput.RunningPressed);
+        }
+    }
+
+    IEnumerator ResetWaiting()
+    {
+        yield return new WaitForSeconds(0.1f);
+        animator.ResetTrigger("jump");
+        isWaiting = false;
+    }
+
+    private void HandleMouseClick()
+    {
+        //AudioManager.instance.PlayButtonClick();
+        Ray playerRay = new Ray(mainCamera.transform.position, mainCamera.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(playerRay, out hit, interactionRayLength, groundMask))
+        {
+            Vector3Int clickedBlockPos = new Vector3Int(
+                Mathf.RoundToInt(hit.point.x),
+                Mathf.RoundToInt(hit.point.y),
+                Mathf.RoundToInt(hit.point.z)
+            );
+
+            if (Input.GetMouseButton(0)) // Left-click: Destroy block
+            {
+                if (clickedBlockPos == lastClickedBlockPos && Time.time - lastClickTime <= doubleClickThreshold)
+                {
+                    ModifyTerrain(hit);
+                }
+                else
+                {
+                    lastClickedBlockPos = clickedBlockPos;
+                    lastClickTime = Time.time;
+                }
+            }
+            else if (Input.GetMouseButton(1)) // Right-click: Place block
+            {
+                BlockType lookedAtBlockType = GetLookedAtBlockType(hit);
+                BlockType blockToPlace = GetNextBlockType(lookedAtBlockType);
+
+                Vector3Int targetBlockPos = new Vector3Int(
+                    Mathf.FloorToInt(hit.point.x - hit.normal.x * 0.5f),
+                    Mathf.FloorToInt(hit.point.y - hit.normal.y * 0.5f),
+                    Mathf.FloorToInt(hit.point.z - hit.normal.z * 0.5f)
+                );
+                Vector3Int placeBlockPos = targetBlockPos + Vector3Int.RoundToInt(hit.normal);
+
+                BlockType existingBlock = world.GetBlockFromChunkCoordinates(
+                    hit.collider.GetComponent<ChunkRenderer>().ChunkData,
+                    placeBlockPos.x, placeBlockPos.y, placeBlockPos.z
+                );
+
+                if (existingBlock == BlockType.Air || existingBlock == BlockType.Nothing)
+                {
+                    world.SetBlock(placeBlockPos, blockToPlace);
+                }
+            }
+        }
+    }
+
+    private BlockType GetNextBlockType(BlockType currentBlock)
+    {
+        switch (currentBlock)
+        {
+            case BlockType.Grass_Dirt: return BlockType.Dirt;
+            case BlockType.Dirt: return BlockType.Stone;
+            case BlockType.Stone: return BlockType.TreeTrunk;
+            case BlockType.TreeTrunk: return BlockType.Grass_Dirt;
+            default: return BlockType.Grass_Dirt;
+        }
+    }
+
+    private void ModifyTerrain(RaycastHit hit)
+    {
+        PlayExplosion(hit.point);
+        world.SetBlock(hit, BlockType.Air);
+    }
+
+    private void PlayExplosion(Vector3 position)
+    {
+        if (explosionPrefab != null)
+        {
+            GameObject explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+            ParticleSystem particles = explosion.GetComponent<ParticleSystem>();
+            particles.Play();
+            Destroy(explosion, particles.main.duration);
+        }
+    }
+
+    private void ToggleInventory()
+    {
+        AudioManager.instance.PlaySFX("Inventory Toggle");
+    }
+
+    private BlockType GetLookedAtBlockType(RaycastHit hit)
+    {
+        Vector3Int blockPos = new Vector3Int(
+            Mathf.RoundToInt(hit.point.x - hit.normal.x / 2),
+            Mathf.RoundToInt(hit.point.y - hit.normal.y / 2),
+            Mathf.RoundToInt(hit.point.z - hit.normal.z / 2)
+        );
+        return world.GetBlockFromChunkCoordinates(
+            hit.collider.GetComponent<ChunkRenderer>().ChunkData,
+            blockPos.x, blockPos.y, blockPos.z
+        );
+    }
+>>>>>>> Stashed changes
 }
--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -2,17 +2,35 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerInput : MonoBehaviour
 {
-	public event Action OnMouseClick, OnFly;
-	public bool RunningPressed { get; private set; }
-	public Vector3 MovementInput { get; private set; }
-	public Vector2 MousePosition { get; private set; }
-	public bool IsJumping { get; private set; }
+	public event Action OnMouseClick, OnMouseRightClick, OnFly, OnInventoryToggle, OnPause;
+    [field: SerializeField] public bool RunningPressed { get; private set; }
+    [field: SerializeField] public Vector3 MovementInput { get; private set; }
+	[field:SerializeField] public Vector2 MousePosition { get; private set; }
+    [field: SerializeField] public bool IsJumping { get; private set; }
 
-    public event Action OnPause;
+    [Header("Touch Input")]
+    public bool useTouchInput;
+    public float touchSensitivity = 0.1f;
+    public float deadZone = 1.5f;
+    int currentLookTouchId;
+    EventSystem eventSystem;
+    InputActions inputActions;
+    private void Awake()
+    {
+        eventSystem = EventSystem.current;
+        InitInput();
+    }
+    private void InitInput()
+    {
+        currentLookTouchId = -1;
+        inputActions = new InputActions();
+        inputActions.Enable();
 
+<<<<<<< Updated upstream
     void Update()
 	{
 		GetMouseClick();
@@ -66,5 +84,85 @@ public class PlayerInput : MonoBehaviour
         {
             OnPause?.Invoke();
         }
+=======
+
+        inputActions.Player.Move.performed += context =>
+        {
+            MovementInput = new(context.ReadValue<Vector2>().x, 0, context.ReadValue<Vector2>().y);
+        };
+        inputActions.Player.Move.canceled += context =>
+        {
+            MovementInput = Vector3.zero;
+        };
+
+        if (!useTouchInput)
+        {
+            inputActions.Player.Look.performed += context =>
+            {
+                MousePosition = new(context.ReadValue<Vector2>().x, context.ReadValue<Vector2>().y);
+            };
+            inputActions.Player.Look.canceled += context =>
+            {
+                MousePosition = Vector2.zero;
+            };
+        }
+        inputActions.Player.Jump.performed += _ => IsJumping = true;
+        inputActions.Player.Jump.canceled += _ => IsJumping = false;
+
+        inputActions.Player.Run.performed += _ => RunningPressed = true;
+        inputActions.Player.Run.canceled += _ => RunningPressed = false;
+
+        inputActions.Player.Fly.performed += _ => OnFly?.Invoke();
+        inputActions.Player.Inventory.performed += _ => OnInventoryToggle?.Invoke();
+        inputActions.Player.Back.performed += _ => OnPause?.Invoke();
+        inputActions.Player.Attack.performed += _ => OnMouseClick?.Invoke();
+        inputActions.Player.Use.performed += _ => OnMouseRightClick?.Invoke();
+    }
+    void Update()
+	{
+        HandleTouchInput();
+    }
+
+    private void HandleTouchInput()
+    {
+        if (!useTouchInput) { return; }
+
+        foreach (var touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began &&
+                !IsTouchOverUI(touch.position) &&
+                currentLookTouchId == -1)
+            {
+                currentLookTouchId = touch.fingerId;
+            }
+
+            if ((touch.phase == TouchPhase.Ended ||
+                touch.phase == TouchPhase.Canceled) &&
+                currentLookTouchId == touch.fingerId)
+            {
+                currentLookTouchId = -1;
+            }
+
+            if (touch.fingerId == currentLookTouchId)
+            {
+                MousePosition = touch.deltaPosition.magnitude > deadZone ?
+                    touch.deltaPosition * touchSensitivity : Vector2.zero;
+            }
+        } 
+
+        if (currentLookTouchId == -1)
+        {
+            MousePosition = Vector2.zero;
+        }
+    }
+
+
+    private bool IsTouchOverUI(Vector2 touchPosition)
+    {
+        PointerEventData eventData = new PointerEventData(eventSystem) { position = touchPosition };
+        List<RaycastResult> results = new List<RaycastResult>();
+        eventSystem.RaycastAll(eventData, results);
+        return results.Count > 0;
+>>>>>>> Stashed changes
     }
 }
